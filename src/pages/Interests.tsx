7b2bb6d64ec5be046f392a89015b5e6d30adac99
Aaron Miller <me.noinc-interview@aaron-miller.me>
2021-03-04 14:57:54 -0500

    Add cards to Skills and Interests pages; adjust responsiveness

--- src/pages/Interests.tsx
+++ src/pages/Interests.tsx
@@ -1,13 +1,20 @@
 import React, { ReactElement } from "react";
+import { useSelector } from "react-redux";
+
+import { Cards } from "../components";
+import { State } from "../reducers";
 
 import "./Interests.css";
 
 export type InterestsProps = unknown;
 
 export function Interests(): ReactElement {
+  const { interests } = useSelector((state: State) => state.data);
+
   return (
     <div className="page interests">
       <h1>Interests</h1>
+      <Cards type={"Interest"} items={interests} />
     </div>
   );
 }
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,13 +1,20 @@
 import React, { ReactElement } from "react";
+import { useSelector } from "react-redux";
+
+import { Cards } from "../components";
+import { State } from "../reducers";
 
 import "./Skills.css";
 
 export type SkillsProps = unknown;
 
 export function Skills(): ReactElement {
+  const { skills } = useSelector((state: State) => state.data);
+
   return (
     <div className="page skills">
       <h1>Skills</h1>
+      <Cards type={"Skill"} items={skills} />
     </div>
   );
 }
